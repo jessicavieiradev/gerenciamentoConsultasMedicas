d64ef6b72c39e4efee3a1fa703570d9061b6ef5e
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-02-23 11:47:39 -0300

    Feat: implementado a classe abstrata do controller pai

---
+++ sistemaAgendamentoMedico/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sistemaAgendamentoMedico.Controllers
+{
+    public abstract class BaseController<T> : ControllerBase
+    {
+        private ILogger<T>? _logger;
+
+        protected ILogger<T> Logger => _logger ??= HttpContext.RequestServices.GetRequiredService<ILogger<T>>()!;
+    }
+}
