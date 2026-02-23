89a97c2bea99e9ae2ab3ff3a9fbf592f980c0ba2
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-02-23 12:56:50 -0300

    feat: centraliza setup do Serilog na pasta de configurações

---
+++ sistemaAgendamentoMedico/Configurations/LoggingConfig.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+
+namespace sistemaAgendamentoMedico.Configurations
+{
+    public static class LoggingConfig
+    {
+        public static void AddSerigLogging(this WebApplicationBuilder builder)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Debug()
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+        }
+    }
+}
