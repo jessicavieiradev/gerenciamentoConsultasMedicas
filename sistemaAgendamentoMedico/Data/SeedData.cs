28654195add8305bdf3d09f957d50593f36f4522
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-02-24 12:35:01 -0300

    feat: adiciona perfis de usuário e lógica de seed

---
+++ sistemaAgendamentoMedico/Data/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using sistemaAgendamentoMedico.Enums;
+
+namespace sistemaAgendamentoMedico.Data
+{
+    public static class SeedData
+    {
+        public static  async Task CriarRolesPadrao(this IHost app)
+        {
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole<long>>>();
+
+            string[] roles = {PerfilUsuario.Medico, PerfilUsuario.Paciente, PerfilUsuario.Admin };
+            
+            foreach (var roleName in roles)
+            {
+                if(!await roleManager.RoleExistsAsync(roleName))
+                {
+                    var result = await roleManager.CreateAsync(new IdentityRole<long>(roleName));
+
+                    if (!result.Succeeded)
+                    {
+                        // Se cair aqui, houve um erro ao inserir
+                        var erro = string.Join(", ", result.Errors.Select(e => e.Description));
+                        throw new Exception($"Erro ao criar role: {erro}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sistemaAgendamentoMedico/Enums/PerfilUsuario.cs
@@ -0,0 +1,9 @@
+﻿namespace sistemaAgendamentoMedico.Enums
+{
+    public static class PerfilUsuario
+    {
+        public const string Admin = "Admin";
+        public const string Medico = "Medico";
+        public const string Paciente = "Paciente";
+    }
+}
