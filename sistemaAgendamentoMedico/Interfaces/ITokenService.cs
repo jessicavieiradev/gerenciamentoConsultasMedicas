87407cb44f47288551c63fada51eb4d4efb01fb3
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-02-27 10:12:05 -0300

    feat: adiciona interface para geracao token

---
+++ sistemaAgendamentoMedico/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using sistemaAgendamentoMedico.Entities;
+
+namespace sistemaAgendamentoMedico.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> GerarToken(Usuario usuario);
+    }
+}
