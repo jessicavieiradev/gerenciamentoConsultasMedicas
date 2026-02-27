64286852937d5711ad5267f9dbc1c6c00140ac43
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-02-27 10:50:15 -0300

    feat: implementado relacionamento de entidades entre paciente e usuario

--- sistemaAgendamentoMedico/Entities/Paciente.cs
+++ sistemaAgendamentoMedico/Entities/Paciente.cs
@@ -3,6 +3,8 @@
     public class Paciente
     {
         public long Id { get; set; }
+        public long UsuarioId { get; set; }
+        public Usuario Usuario { get; set; } = null!;
         public string NomeCompleto { get; set; } = string.Empty;
         public string Cpf { get; set; } = string.Empty;
         public DateOnly DataNascimento { get; set; } 
