9c3a9f7d43e3c040a714b7a480d9e38b77a4d152
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-05 13:36:14 -0300

    refactor: modifica a estrutura adicionando uma nova coluna

--- sistemaAgendamentoMedico/Data/Configurations/MedicoConfiguration.cs
+++ sistemaAgendamentoMedico/Data/Configurations/MedicoConfiguration.cs
@@ -9,14 +9,46 @@ namespace sistemaAgendamentoMedico.Data.Configurations
         public void Configure(EntityTypeBuilder<Medico> builder)
         {
             builder.ToTable("medico");
+
             builder.HasKey(x => x.Id);
 
-            builder.Property(x => x.NomeCompleto).HasColumnName("nome_completo").HasMaxLength(200).IsRequired();
-            builder.Property(x => x.Crm).HasColumnName("crm").HasMaxLength(10).IsRequired();
-            builder.HasIndex(x => x.Crm).IsUnique();
+            builder.Property(x => x.NomeCompleto)
+                .HasColumnName("nome_completo")
+                .HasMaxLength(200)
+                .IsRequired();
 
-            builder.Property(x => x.Especialidade).HasMaxLength(50).IsRequired();
-            builder.Property(x => x.Ativo).IsRequired();
+            builder.Property(x => x.Especialidade)
+                .HasColumnName("especialidade")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.Crm)
+                .HasColumnName("crm")
+                .HasMaxLength(15)
+                .IsRequired();
+
+            builder.Property(x => x.Uf)
+                .HasColumnName("uf")
+                .HasMaxLength(2)
+                .IsRequired();
+
+            builder.Property(x => x.Cpf)
+                .HasColumnName("cpf")
+                .HasMaxLength(11)
+                .IsRequired();
+
+            builder.Property(x => x.Ativo)
+                .HasColumnName("ativo")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.HasIndex(x => x.Cpf).IsUnique();
+            builder.HasIndex(x => new { x.Crm, x.Uf }).IsUnique();
+
+            builder.HasOne(x => x.Usuario)
+                .WithOne()
+                .HasForeignKey<Medico>(x => x.UsuarioId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
--- sistemaAgendamentoMedico/Entities/Medico.cs
+++ sistemaAgendamentoMedico/Entities/Medico.cs
@@ -4,8 +4,12 @@
     {
         public long Id { get; set; }
         public string NomeCompleto { get; set; } = string.Empty;
-        public string Crm { get; set; } = string.Empty;
         public string Especialidade { get; set; } = string.Empty;
+        public string Crm { get; set; } = string.Empty;
+        public string Uf { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public long UsuarioId { get; set; }
+        public Usuario Usuario { get; set; } = null!;
         public bool Ativo { get; set; } = true;
     }
 }
